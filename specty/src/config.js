3b4187455518a31008ee3afe6473d42c67e7730d
mesiik <71527914+mesiik@users.noreply.github.com>
2021-05-24 13:06:28 +0200

    👀

---
+++ specty/src/config.js
@@ -0,0 +1,17 @@
+const config = {
+    bot: {
+        token: 'Nb298ruvgfutvfFityfk2U6F7DIE51TUKV,HEWFVIUL4.FBG274IEHGF2Y78O1Y3jbuefwgl',
+        prefix: 's.',
+        owners: ['563718132863074324', '423551504952066051', '338075554937044994']
+    },
+    Embeds: {
+        colors: {
+            Main: '#6b94ec',
+            Errors: '#d4472e'
+        },
+        legal: {
+            website: 'https://bots4.fun',
+            discord: ' https://discord.gg/Zbfvh88dgK'
+        }
+    }
+}
